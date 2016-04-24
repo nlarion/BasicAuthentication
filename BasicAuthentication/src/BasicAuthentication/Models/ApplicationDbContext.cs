61a1b4d0b36591f89c5d97f44ff8176e07ee5c7e
Neil Larion <neil.larion@gmail.com>
2016-04-24 14:37:07 -0700

    Get Basic login authentication working

---
+++ BasicAuthentication/src/BasicAuthentication/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BasicAuthentication.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
